b8ba2ac1dc5bb1c69ddf9b48d5a527d526f4c897
Vicente Hernández Maciá <vhm0006@alu.medac.es>
2024-04-17 13:11:36 +0200

    Hibernate añadido
    Configuración de ficheros pom.xml y conexión a la base de datos.

---
+++ UpdateHibernate/src/main/java/org/example/Main.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+        Configuration configuration = new Configuration();
+        configuration.configure("hibernate.cfg.xml");
+
+        // Create Session Factory
+        SessionFactory sessionFactory
+                = configuration.buildSessionFactory();
+
+        // Initialize Session Object
+        Session session = sessionFactory.openSession();
+    }
+}
